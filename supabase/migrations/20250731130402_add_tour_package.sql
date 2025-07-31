50513bad751a26d1c0a02806deeca00009262a7b
pradanaadn <pradanaadn21@gmail.com>
2025-07-31 21:18:29 +0800

    fix: correct place_id type in user_place_rating table and remove unique constraint from tour_package table

--- supabase/migrations/20250731130402_add_tour_package.sql
+++ supabase/migrations/20250731130402_add_tour_package.sql
@@ -2,7 +2,6 @@ create table if not exists public.tour_package (
     id uuid primary key default gen_random_uuid(),
     tour_package_id int not null,
     place_id int not null,
-    unique (tour_package_id, place_id),
     created_at timestamp with time zone default timezone('utc' :: text, now()),
     updated_at timestamp with time zone default timezone('utc' :: text, now()),
     deleted_at timestamp with time zone null
