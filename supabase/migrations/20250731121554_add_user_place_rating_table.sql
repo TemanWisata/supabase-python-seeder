6913082f1e6a26b7d16cb9743c7d62b6a5af0019
pradanaadn <pradanaadn21@gmail.com>
2025-07-31 21:09:24 +0800

    chore: add migration and seeder for user place rating table and tour_package

---
+++ supabase/migrations/20250731121554_add_user_place_rating_table.sql
@@ -0,0 +1,13 @@
+create table if not exists public.user_place_rating (
+    id uuid primary key default gen_random_uuid(),
+    user_id int not null,
+    place_id not null,
+    rating numeric(2, 1) not null check (
+        rating >= 0
+        and rating <= 5
+    ),
+    unique (user_id, place_id),
+    created_at timestamp with time zone default timezone('utc' :: text, now()),
+    updated_at timestamp with time zone default timezone('utc' :: text, now()),
+    deleted_at timestamp with time zone null
+);
